e70ee7d47f0e33c3b6b98e4a8f28b97be9ee9926
HelioStrike <rageofepicfury@gmail.com>
2019-12-31 11:11:50 +0530

    added prog descs

--- confusingNumbers.cpp
+++ confusingNumbers.cpp
@@ -1,3 +1,26 @@
+/*
+We can rotate digits by 180 degrees to form new digits. When 0, 1, 6, 8, 9 are rotated 180 degrees, they become 0, 1, 9, 8, 6 respectively. When 2, 3, 4, 5 and 7 are rotated 180 degrees, they become invalid.
+
+A confusing number is a number that when rotated 180 degrees becomes a different number with each digit valid.
+(Note that the rotated number can be greater than the original number.)
+
+Given a positive integer N, return the number of confusing numbers between 1 and N inclusive.
+
+ 
+ 
+ 
+Input: 20
+Output: 6
+Explanation: 
+The confusing numbers are [6,9,10,16,18,19].
+6 converts to 9.
+9 converts to 6.
+10 converts to 01 which is just 1.
+16 converts to 91.
+18 converts to 81.
+19 converts to 61.
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
