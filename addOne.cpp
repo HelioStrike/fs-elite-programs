0b503a3eb1410d4d9715b6bf85de1ed990f50286
Tanmay Thakur <tanmaythakur2025@gmail.com>
2020-03-03 07:09:29 +0530

    Added solution to addone.cpp

--- addOne.cpp
+++ addOne.cpp
@@ -19,3 +19,29 @@ Example 2:
 	Explanation: The array represents the integer 4321.
 */
 
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+  vector <int> v;
+  string str;
+  getline(cin, str);
+
+  for(int i=0; i<str.length(); i+=2)
+    v.push_back(str[i]-'0');
+
+  int i = v.size() - 1;
+
+  while(v[i] == 9) v[i] = 0, i--;
+
+  
+  if(i==-1) cout << 1 << "";   // when 9, 999, etc are inputs, the next num is 10, 1000 etc
+  else v[i] += 1;
+
+
+  for(auto i: v) cout << i << "";
+
+  return 0;
+}
